e2a99a1c59228e9339535992bdc6bb3526a6efcf
Ethan Chen <intervigil@gmail.com>
2012-06-10 19:42:09 -0700

    ville: inherit from msm8960-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,28 +24,11 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := device/htc/ville/include
+# inherit from common msm8960
+-include device/htc/msm8960-common/BoardConfigCommon.mk
 
 # Bootloader
-TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := ville
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-
-# Architecture
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80400000
@@ -57,43 +40,9 @@ TARGET_PREBUILT_KERNEL := device/htc/ville/prebuilt/kernel
 TARGET_KERNEL_CONFIG := ville_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
-# Init
-TARGET_PROVIDES_INIT_RC := true
-
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
-
-# Audio
-COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
-BOARD_USES_ALSA_AUDIO := true
-TARGET_USES_ION_AUDIO := true
-TARGET_USES_QCOM_LPA := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-TARGET_CUSTOM_BLUEDROID := ../../../device/htc/ville/bluetooth/bluetooth.c
-
-# Camera
-#BOARD_HAVE_HTC_FFC := true
-
-# FM radio
-#BOARD_HAVE_FM_RADIO := true
-#BOARD_GLOBAL_CFAGS += -DHAVE_FM_RADIO
-
 # QCOM GPS
-#BOARD_USES_QCOM_GPS := true
 #BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := ville
-
-# Graphics
-COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
-USE_OPENGL_RENDERER := true
-TARGET_HAVE_BYPASS := false
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-TARGET_USES_OVERLAY := true
-TARGET_QCOM_HDMI_OUT := true
-BOARD_EGL_CFG := device/htc/ville/configs/egl.cfg
 
 # Lights
 TARGET_PROVIDES_LIBLIGHTS := true
@@ -102,20 +51,8 @@ TARGET_PROVIDES_LIBLIGHTS := true
 BOARD_PROVIDES_LIBRIL := true
 
 # Wifi
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_WLAN_DEVICE                := qcwcn
-TARGET_CUSTOM_WIFI               := ../../device/htc/ville/wifi/wifi.c
-WIFI_DRIVER_FW_PATH_STA          := "sta"
-WIFI_DRIVER_FW_PATH_AP           := "ap"
-WIFI_DRIVER_FW_PATH_P2P          := "p2p"
 WIFI_DRIVER_MODULE_NAME          := prima_wlan
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/prima_wlan.ko"
-
-# Webkit
-TARGET_FORCE_CPU_UPLOAD := true
-DYNAMIC_SHARED_LIBV8SO := true
 
 # cat /proc/emmc
 #dev:        size     erasesize name
--- device_ville.mk
+++ device_ville.mk
@@ -17,27 +17,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# common msm8960 configs
+$(call inherit-product, device/htc/msm8960-common/msm8960.mk)
+
 DEVICE_PACKAGE_OVERLAYS += device/htc/ville/overlay
 
 # Boot ramdisk setup
 PRODUCT_COPY_FILES += \
-    device/htc/ville/prebuilt/init:root/init \
-    device/htc/ville/ramdisk/init.qcom.sh:root/init.qcom.sh \
-    device/htc/ville/ramdisk/init.rc:root/init.rc \
     device/htc/ville/ramdisk/init.ville.rc:root/init.ville.rc \
     device/htc/ville/ramdisk/init.ville.usb.rc:root/init.ville.usb.rc \
     device/htc/ville/ramdisk/ueventd.ville.rc:root/ueventd.ville.rc
-
-# Qualcomm scripts
-PRODUCT_COPY_FILES += \
-    device/htc/ville/configs/init.qcom.bt.sh:/system/etc/init.qcom.bt.sh \
-    device/htc/ville/configs/init.qcom.fm.sh:/system/etc/init.qcom.fm.sh \
-    device/htc/ville/configs/init.qcom.post_boot.sh:/system/etc/init.qcom.post_boot.sh \
-    device/htc/ville/configs/init.qcom.q6_links.sh:/system/etc/init.qcom.q6_links.sh \
-    device/htc/ville/configs/init.qcom.radio_links.sh:/system/etc/init.qcom.radio_links.sh \
-    device/htc/ville/configs/init.qcom.sdio.sh:/system/etc/init.qcom.sdio.sh \
-    device/htc/ville/configs/init.qcom.wcnss_links.sh:/system/etc/init.qcom.wcnss_links.sh \
-    device/htc/ville/configs/init.qcom.wifi.sh:/system/etc/init.qcom.wifi.sh
 
 # recovery and custom charging
 PRODUCT_COPY_FILES += \
@@ -51,13 +40,13 @@ PRODUCT_COPY_FILES += device/common/gps/gps.conf_US:system/etc/gps.conf
 
 # Media config
 PRODUCT_COPY_FILES += \
-    device/htc/ville/configs/media_profiles.xml:system/etc/media_profiles.xml
+    device/htc/msm8960-common/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # HTC BT audio config
 PRODUCT_COPY_FILES += device/htc/ville/configs/AudioBTID.csv:system/etc/AudioBTID.csv
 
 # QC thermald config
-PRODUCT_COPY_FILES += device/htc/ville/configs/thermald.conf:system/etc/thermald.conf
+PRODUCT_COPY_FILES += device/htc/msm8960-common/configs/thermald.conf:system/etc/thermald.conf
 
 # vold config
 PRODUCT_COPY_FILES += \
@@ -113,26 +102,6 @@ PRODUCT_COPY_FILES += \
     device/htc/ville/idc/projector_input.idc:system/usr/idc/projector_input.idc \
     device/htc/ville/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc
 
-# Audio
-PRODUCT_PACKAGES += \
-    alsa.msm8960 \
-    audio.a2dp.default \
-    audio_policy.msm8960 \
-    audio.primary.msm8960 \
-    libalsa-intf \
-    libaudioutils
-
-# Graphics
-PRODUCT_PACKAGES += \
-    copybit.msm8960 \
-    gralloc.msm8960 \
-    hwcomposer.msm8960 \
-    libgenlock \
-    libmemalloc \
-    liboverlay \
-    libQcomUI \
-    libtilerenderer
-
 # GPS
 #PRODUCT_PACKAGES += \
 #    gps.ville \
@@ -141,77 +110,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.ville
 
-# OMX
-PRODUCT_PACKAGES += \
-    libdivxdrmdecrypt \
-    libmm-omxcore \
-    libOmxCore \
-    libOmxVdec \
-    libOmxVenc \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libstagefrighthw
-
-# HDMI
-PRODUCT_PACKAGES += \
-    hdmid
-
 # Torch
 PRODUCT_PACKAGES += \
-		Torch
-
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
-
-# Live wallpapers
-PRODUCT_PACKAGES += \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers \
-    librs_jni
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    make_ext4fs \
-    setup_fs
+    Torch
 
 # Kernel modules
 PRODUCT_COPY_FILES += \
     device/htc/ville/modules/prima_wlan.ko:/system/lib/modules/prima_wlan.ko
 
-# MSM8960 firmware
-PRODUCT_COPY_FILES += \
-    device/htc/ville/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
-    device/htc/ville/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
-    device/htc/ville/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
-    device/htc/ville/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
-    device/htc/ville/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
-    device/htc/ville/firmware/vidc_1080p.fw:/system/etc/firmware/vidc_1080p.fw
-    #device/htc/ville/firmware/wcd9310_anc.bin:/system/etc/firmware/wcd9310_anc.bin
-
-# Wifi firmware
-PRODUCT_COPY_FILES += \
-    device/htc/ville/firmware/WCNSS_cfg.dat:/system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    device/htc/ville/firmware/WCNSS_qcom_cfg.ini:/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
-    device/htc/ville/firmware/WCNSS_qcom_wlan_nv.bin:/system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardwardware.sensor.gyroscope.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # Extra properties
