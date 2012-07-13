89dcc2dfc26de02e25a0df890e6deebd512d6268
Ethan Chen <intervigil@gmail.com>
2012-07-12 22:54:27 -0700

    ville: fix liblight for JB

--- liblights/lights.c
+++ liblights/lights.c
@@ -78,7 +78,7 @@ static int write_int(const char* path, int value) {
   fd = open(path, O_RDWR);
   if (fd < 0) {
     if (already_warned == 0) {
-      LOGE("write_int failed to open %s\n", path);
+      ALOGE("write_int failed to open %s\n", path);
       already_warned = 1;
     }
     return -errno;
@@ -145,7 +145,7 @@ static void set_speaker_light_locked(struct light_device_t *dev,
           write_int(GREEN_BLINK_FILE, 0);
           break;
         default:
-          LOGE("set_led_state colorRGB=%08X, unknown color\n", colorRGB);
+          ALOGE("set_led_state colorRGB=%08X, unknown color\n", colorRGB);
           break;
       }
       break;
@@ -166,8 +166,8 @@ static void set_speaker_light_locked(struct light_device_t *dev,
       }
       break;
     default:
-      LOGE("set_led_state colorRGB=%08X, unknown mode %d\n",
-           colorRGB, state->flashMode);
+      ALOGE("set_led_state colorRGB=%08X, unknown mode %d\n",
+            colorRGB, state->flashMode);
   }
 }
 
@@ -193,7 +193,7 @@ static void set_speaker_light_locked_dual(struct light_device_t *dev,
       write_int (GREEN_BLINK_FILE, 4);
       break;
     default:
-      LOGE("set_led_state (dual) unexpected color: bcolorRGB=%08x\n", bcolorRGB);
+      ALOGE("set_led_state (dual) unexpected color: bcolorRGB=%08x\n", bcolorRGB);
   }
 }
 
@@ -230,7 +230,7 @@ static int set_light_backlight(struct light_device_t* dev,
                                struct light_state_t const* state) {
   int err = 0;
   int brightness = rgb_to_brightness(state);
-  LOGV("%s brightness=%d color=0x%08x", __func__,brightness, state->color);
+  ALOGV("%s brightness=%d color=0x%08x", __func__,brightness, state->color);
   pthread_mutex_lock(&g_lock);
   g_backlight = brightness;
   err = write_int(LCD_BACKLIGHT_FILE, brightness);
@@ -309,7 +309,7 @@ static struct hw_module_methods_t lights_module_methods = {
   .open = open_lights,
 };
 
-const struct hw_module_t HAL_MODULE_INFO_SYM = {
+struct hw_module_t HAL_MODULE_INFO_SYM = {
   .tag = HARDWARE_MODULE_TAG,
   .version_major = 1,
   .version_minor = 0,
