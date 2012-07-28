c4db8ddec69fc2f89056a1c14be1c58cf8d387eb
Ethan Chen <intervigil@gmail.com>
2012-07-28 16:59:24 -0700

    ville: move kernel source define to msm8960-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ BOARD_FORCE_RAMDISK_ADDRESS := 0x81800000
 
 TARGET_PREBUILT_KERNEL := device/htc/ville/prebuilt/kernel
 TARGET_KERNEL_CONFIG := ville_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # ICS Compat things
 BOARD_NEEDS_MEMORYHEAPPMEM := true
