9149644b077ebdc2477d9a50a1d71e21cad5b536
Ethan Chen <intervigil@gmail.com>
2012-08-21 17:38:57 -0700

    ville: update CM10 boot animation name
    Change-Id: If0e1aabc546dea4d7fc085f42430dec1e30f4800

--- cm.mk
+++ cm.mk
@@ -4,8 +4,6 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := ville
 
-TARGET_BOOTANIMATION_NAME := vertical-540x960
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
