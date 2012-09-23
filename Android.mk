42d20f00c419074acd3492b4000cb1b009ddf932
Giulio Cervera <giulio.cervera@gmail.com>
2012-09-23 02:58:47 +0200

    ville: add build barrier
    Change-Id: I15979195e81fe1085ca49c34756450ab117f3aef

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2010 The Android Open Source Project
+#
+# Copyright (C) 2012 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),ville)
+    include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
