e5ee44ce6139381576b30edeca12ef5baa1d3511
Ethan Chen <intervigil@gmail.com>
2013-01-19 18:29:01 -0800

    ville: move init scripts to common where possible
    Change-Id: I61c31425f106b1216fa07ba54140f4617a7140fb

--- device_ville.mk
+++ device_ville.mk
@@ -22,11 +22,9 @@ $(call inherit-product, device/htc/msm8960-common/msm8960.mk)
 DEVICE_PACKAGE_OVERLAYS += device/htc/ville/overlay
 
 # Boot ramdisk setup
-PRODUCT_COPY_FILES += \
-    device/htc/ville/ramdisk/fstab.ville:root/fstab.ville \
-    device/htc/ville/ramdisk/init.ville.rc:root/init.ville.rc \
-    device/htc/ville/ramdisk/init.ville.usb.rc:root/init.ville.usb.rc \
-    device/htc/ville/ramdisk/ueventd.ville.rc:root/ueventd.ville.rc
+PRODUCT_PACKAGES += \
+    fstab.ville \
+    init.target.rc
 
 # recovery and custom charging
 PRODUCT_COPY_FILES += \
