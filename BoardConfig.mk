f3d20632368a0943e426c630503d2baf31372805
Matt Mower <mowerm@gmail.com>
2013-05-14 18:14:36 -0500

    Recovery: remove unnecessary BoardConfig parameters
    BOARD_SUPPRESS_EMMC_WIPE: according to http://review.cyanogenmod.org/17326 ,
    this is only relevant to Samsung devices with buggy firmware
    
    BOARD_VOLD_EMMC_SHARES_DEV_MAJOR: already defined in the # Vold section
    
    BOARD_USES_MMCUTIL: cannot find source that utilizes this
    
    Change-Id: I3bc5417e27bbd326b36f63be6384a24b25b70a5b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,4 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Recovery
-BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_USES_MMCUTILS := true
 TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/ville/recovery/kernel
