29b054d7e32bc40c7a9436feb45b761c40b5b86f
Ethan Chen <intervigil@gmail.com>
2013-08-22 12:08:16 -0700

    ville: bluetooth: enable QCOM WCNSS features
    Change-Id: Ia90f5b7f09db158e869e84891a923fdc0c427727

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,5 +19,8 @@
 
 #define BTM_DEF_LOCAL_NAME   "One S"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BLUETOOTH_QCOM_SW TRUE
+#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
+#define BTC_INCLUDED TRUE
 
 #endif
