085f4dd66ead8382ddbc3cfb8915fe90dbcbdd11
Flemmard <flemmard@gmail.com>
2013-11-14 17:53:04 +0100

    ville: change kernel config from ville_defconfig to cyanogenmod_ville_defconfig Only use this if needed. DO NOT MERGE FOR NOW
    Change-Id: I33aa6bf0d2888c051f749c5545a63ef26734f992

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_BOARD_INFO_FILE ?= device/htc/ville/board-info.txt
 TARGET_BOOTLOADER_BOARD_NAME := ville
 
 # Kernel
-TARGET_KERNEL_CONFIG := ville_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_ville_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/ville/bluetooth
