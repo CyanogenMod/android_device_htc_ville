b5ccbd6538d469ac18620d3b79f85a93f9bb3876
Ethan Chen <intervigil@gmail.com>
2014-07-04 11:48:14 -0700

    ville: Use smaller recovery font
    * Ville is not 720p, so should use the smaller font.
    
    Change-Id: I4bb2a3c35b8147325df500b8dc26d7f43ba0eb0a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,4 +79,5 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_VOLD_MAX_PARTITIONS := 36
 
 # Recovery
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
 TARGET_RECOVERY_FSTAB := device/htc/ville/rootdir/etc/fstab.qcom
