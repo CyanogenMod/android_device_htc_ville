58145055ab4289f871185644faf2eeba5194758c
JoseGalRe <josegalre@gmail.com>
2015-01-11 10:39:40 -0600

    Remove BUILD_UTC_DATE
    * Already is defined in the common configuration
      https://github.com/CyanogenMod/android_vendor_cm/blob/cm-12.0/config/common.mk#L47
    
    Change-Id: I5a9e33aae737d1723c64b11289e45ba07d8c3824

--- device_ville.mk
+++ device_ville.mk
@@ -72,9 +72,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.locationfeatures=1 \
     dalvik.vm.dexopt-flags=m=y
 
-# Set build date
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
