1a31f7f7e6e1f1255b284da736d075e2f6616246
Ethan Chen <intervigil@gmail.com>
2016-06-10 13:23:46 -0700

    ville: Remove redundant languages inclusion
    * Transitively included by full_base_telephony
    
    Change-Id: Ia3d84404b24391dabb23d9f27ef9aaa792a642e2

--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/htc/ville/ville-vendor.mk)
 
